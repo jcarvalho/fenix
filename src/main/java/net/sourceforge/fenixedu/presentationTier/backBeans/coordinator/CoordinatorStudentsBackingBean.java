a8df55ce8d36eb2d65e58c57e62d8e2201cf5933
Pedro Santos <pedro.san7os@gmail.com>
2014-04-14 15:33:46 +0100

    SearchDegreeStudentsGroup is not a group
    Conflicts:
    	src/main/java/net/sourceforge/fenixedu/domain/accessControl/SearchDegreeStudentsGroup.java

--- src/main/java/net/sourceforge/fenixedu/presentationTier/backBeans/coordinator/CoordinatorStudentsBackingBean.java
+++ src/main/java/net/sourceforge/fenixedu/presentationTier/backBeans/coordinator/CoordinatorStudentsBackingBean.java
@@ -307,7 +307,7 @@ public class CoordinatorStudentsBackingBean extends FenixBackingBean {
 
     public String getSerializedFilteredStudents() {
         SearchDegreeStudentsGroup searchGroup = getSearchCriteriaGroup();
-        return searchGroup.getExpressionInHex();
+        return searchGroup.serialize();
     }
 
     public StudentCurricularPlanState getStudentCurricularPlanState() {
